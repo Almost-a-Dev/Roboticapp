e90d965a7805378a5f493632abb92de7c3a5cc4c
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2020-04-16 18:43:03 -0400

    Add logic to search a robot using OnChange event... next delivery with some style improvement...

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component, Fragment } from 'react';
 import CardList from './components/CardList';
 import Header from './components/shared/Header/Header';
 import Footer from './components/shared/Footer/Footer';
+import InputForm from "./components/shared/InputForm/InputForm";
 import { robots } from "./robots";
 import './App.css';
 
 class App extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchTerm: ''
+    }
+  }
+
+  inputChange = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <Fragment>
         <Header />
+        <InputForm inputChange={this.inputChange}/>
         <div className="App">
           <CardList robots={robots} />
         </div>
